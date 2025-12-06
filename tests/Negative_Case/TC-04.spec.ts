d855d3890bbd84c0801707def69552b7fa9e1750
Bintang <prasetyobintangg@gmail.com>
2025-12-06 18:03:31 +0700

    adding TC-04, TC-05, and update excel test case

---
+++ tests/Negative_Case/TC-04.spec.ts
@@ -0,0 +1,30 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('TC-04 - Negative Login', () => {
+    test('User gagal login menggunakan username salah dan password valid', async ({ page }) => {
+        // Identify Credential:
+        const username = 'username';
+        const password = 'secret_sauce';
+
+        // Step 1: Navigate to the login page
+        await page.goto('https://www.saucedemo.com/');
+
+        // Step 2: empty username and valid password
+        await page.fill('#user-name', '');
+        // Optional Screenshot after filling username
+        await page.screenshot({ path: 'screenshots/TC-04-username-filled.png' });
+
+        await page.fill('#password', password);
+        // Optional Screenshot after filling password
+        await page.screenshot({ path: 'screenshots/TC-04-password-filled.png' });
+        // Step 3: Click the login button
+        await page.click('#login-button');
+
+        // Step 4: Verify unsuccessful login by checking the error message
+        const errorMessage = page.locator('[data-test="error"]');
+        await expect(errorMessage).toBeVisible();
+        await expect(errorMessage).toHaveText('Epic sadface: Username is required');
+        // Take a screenshot of the error message
+        await page.screenshot({ path: 'screenshots/TC-04-login-failed-username-empty.png' });
+    })
+})
---
+++ tests/Negative_Case/TC-05.spec.ts
@@ -0,0 +1,34 @@
+// Import necessary modules and dependencies
+import { test, expect } from '@playwright/test';
+
+// Define the test case for negative scenario
+test.describe('TC-05 - Negative Login', () => {
+    // Define the individual test
+    // Empty password field with valid username
+    test('User gagal login menggunakan username valid dan password kosong', async ({ page }) => {
+        // Identify Credential: 
+        const username = 'standard_user';
+        const password = '';
+
+        // Step 1: Navigate to the login page
+        await page.goto('https://www.saucedemo.com/');
+
+        // Step 2: valid username and empty password
+        await page.fill('#user-name', username);
+        // Optional Screenshot after filling username
+        await page.screenshot({ path: 'screenshots/TC-05-username-filled.png' });
+
+        await page.fill('#password', '');
+        // Optional Screenshot after filling password
+        await page.screenshot({ path: 'screenshots/TC-05-password-filled.png' });
+
+        // Step 3: Click the login button
+        await page.click('#login-button');
+        // Step 4: Verify unsuccessful login by checking the error message
+        const errorMessage = page.locator('[data-test="error"]');
+        await expect(errorMessage).toBeVisible();
+        await expect(errorMessage).toHaveText('Epic sadface: Password is required');
+        // Take a screenshot of the error message
+        await page.screenshot({ path: 'screenshots/TC-05-login-failed-password-empty.png' });
+    })
+})
